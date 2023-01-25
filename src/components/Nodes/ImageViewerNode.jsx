12fd25ffcf946b9df60d2f6a08a836d539ea6829
Florian  Valade <florian_val@outlook.fr>
2023-01-25 14:05:36 +0100

    add simple image viewer node

---
+++ src/components/Nodes/ImageViewerNode.jsx
@@ -0,0 +1,12 @@
+import React, { memo } from "react";
+import { Handle } from "reactflow";
+
+export default memo(({ data }) => {
+    return (
+        <div className="react-flow__node-input">
+            <Handle type="target" position="left" id="input" isConnectable={true} />
+            {data.input ? <img src={data.input} alt="Image" style={{ width: "100px", height: "100px" }} /> : <div>Connect me to display data</div>}
+        </div>
+    );
+}
+);
--- src/ressources/nodeTypes.js
+++ src/ressources/nodeTypes.js
@@ -1,17 +1,20 @@
 import CustomNode from '../components/Nodes/CustomNode';
 import ImageInputNode from '../components/Nodes/ImageInputNode';
 import CannyNode from '../components/Nodes/CannyNode';
+import ImageViewerNode from '../components/Nodes/ImageViewerNode';
 
 export const nodeTypes = {
     custom: CustomNode,
     image_input: ImageInputNode,
     canny: CannyNode,
+    image_viewer: ImageViewerNode,
 };
 
 export const nodeTitles = {
     custom: 'Custom Node',
     image_input: 'Image Input Node',
     canny: 'Canny Node',
+    image_viewer: 'Image Viewer Node',
 };
 
 export default nodeTypes;
