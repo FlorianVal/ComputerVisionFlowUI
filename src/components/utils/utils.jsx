1192fd9d620cf1889cf3a70f03d12823b197b73a
Florian  Valade <florian_val@outlook.fr>
2023-01-26 17:56:03 +0100

    add constraints on nodes

--- src/components/utils/utils.jsx
+++ src/components/utils/utils.jsx
@@ -1,15 +1,29 @@
 import { Handle } from "reactflow";
-import { nodeHasHandle } from "../../ressources/nodeTypes";
+import { nodeHasHandle, nodeConstraints } from "../../ressources/nodeTypes";
+import { useStoreApi } from "reactflow";
+
 
 export const WrapHandles = ({ children, type, className }) => {
+
+    const store = useStoreApi();
+
     return (
-        <div className={className}>
-            {nodeHasHandle[type].inputs ? <Handle type="target" position="left" /> : <div></div>}
-            <div>
+
+        < div className={className} >
+            {
+                nodeHasHandle[type].inputs ? <Handle type="target" position="left" isValidConnection={(connection) =>
+                    nodeConstraints[store.getState().getNodes().find(node => node.id === connection.source).type]["outputs"] === nodeConstraints[type]["inputs"]
+                } /> : <div></div>
+            }
+            < div >
                 {children}
-            </div>
-            {nodeHasHandle[type].outputs ? <Handle type="source" position="right" /> : <div></div>}
-        </div>
+            </div >
+            {
+                nodeHasHandle[type].outputs ? <Handle type="source" position="right" isValidConnection={(connection) =>
+                    nodeConstraints[store.getState().getNodes().find(node => node.id === connection.target).type]["inputs"] === nodeConstraints[type]["outputs"]
+                } /> : <div></div>
+            }
+        </div >
     )
 }
 
