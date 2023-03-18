020bae4aebbacedde907c369efdbcaaff9629126
Florian  Valade <florian_val@outlook.fr>
2023-03-18 15:06:53 +0100

    optimize node menu to make it functionnal

--- src/components/AddNodeMenu.jsx
+++ src/components/AddNodeMenu.jsx
@@ -1,51 +1,52 @@
 import { DownOutlined } from '@ant-design/icons';
-import { Button, Dropdown, Space } from 'antd';
-import ReactFlow, {
-    ControlButton,
-} from 'reactflow';
-import { nodeTypes, nodeTitles } from '../ressources/nodeTypes';
+import { Dropdown } from 'antd';
+import { ControlButton } from 'reactflow';
+import { nodeTitles } from '../ressources/nodeTypes';
 import React from 'react';
 
-class AddMenuDropdown extends React.Component {
-    constructor(props) {
-        super(props);
-        this.onDragStart = this.onDragStart.bind(this);
-        this.createNodeTypeList = this.createNodeTypeList.bind(this);
-        this.items = this.createNodeTypeList(nodeTypes);
-    }
-
-    createNodeTypeList() {
+const AddMenuDropdown = () => {
+    const createNodeTypeList = () => {
         return Object.entries(nodeTitles).map(([key, value], index) => {
             return {
-                label: <a draggable onDragStart={(event) => this.onDragStart(event, key)}>{value}</a>,
-                key: index.toString()
-            }
-        })
-    }
+                label: (
+                    <a
+                        draggable
+                        onDragStart={(event) => onDragStart(event, key)}
+                    >
+                        {value}
+                    </a>
+                ),
+                key: index.toString(),
+            };
+        });
+    };
 
-    onDragStart(event, nodeType) {
+    const items = createNodeTypeList();
+
+    const onDragStart = (event, nodeType) => {
         console.log('onDragStart', nodeType);
         event.dataTransfer.setData('application/reactflow', nodeType);
         event.dataTransfer.effectAllowed = 'move';
-    }
+    };
 
-    render() {
-        return (
-            <Dropdown
-                menu={{
-                    items: this.items,
-                }}
-                trigger={['click']}
-                align={{ offset: [40, -20] }}
-            >
-                <a onClick={(e) => e.preventDefault()}>
-                    <ControlButton>
-                        <div><font size="+2">+</font></div>
-                        <DownOutlined />
-                    </ControlButton>
-                </a>
-            </Dropdown >
-        )
-    }
-}
+    return (
+        <Dropdown
+            menu={{
+                items: items,
+            }}
+            trigger={['click']}
+            align={{ offset: [40, -20] }}
+        >
+            <a onClick={(e) => e.preventDefault()}>
+                <ControlButton>
+                    <div>
+                        <font size="+2">+</font>
+                    </div>
+                    <DownOutlined />
+                </ControlButton>
+            </a>
+        </Dropdown>
+    );
+};
+
 export default AddMenuDropdown;
